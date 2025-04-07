f71144cf162258eab065c7ce6978b14b1b817215
Prayush Dave <prayush.dave@stonybrook.edu>
2025-04-07 16:02:23 -0400

    Add Sample Data

---
+++ data/medium/medium.pl
@@ -0,0 +1,37 @@
+UCLA pl 1.0
+# Created: 2025-03-30 22:52:00
+# User request: Medium size (30 nodes, 20 nets) test case
+
+# Movable Cells (Placed somewhat randomly) - name x y : orientation /PLACED
+c1   10  15 : N /PLACED
+c2   20  25 : N /PLACED
+c3   30  35 : N /PLACED
+c4   40  45 : N /PLACED
+c5   50  55 : N /PLACED
+c6   60  65 : N /PLACED
+c7   70  75 : N /PLACED
+c8   80  85 : N /PLACED
+c9   15  80 : N /PLACED
+c10  25  70 : N /PLACED
+c11  35  60 : N /PLACED
+c12  45  50 : N /PLACED
+c13  55  40 : N /PLACED
+c14  65  30 : N /PLACED
+c15  75  20 : N /PLACED
+c16  85  10 : N /PLACED
+c17  20  90 : N /PLACED
+c18  40  10 : N /PLACED
+c19  60  90 : N /PLACED
+c20  80  10 : N /PLACED
+
+# Fixed Terminals (Placed around periphery 0-100) - name x y : orientation /FIXED
+p1    0  50 : N /FIXED
+p2  100  50 : N /FIXED
+p3   50   0 : N /FIXED
+p4   50 100 : N /FIXED
+p5    0  25 : N /FIXED
+p6  100  25 : N /FIXED
+p7   25   0 : N /FIXED
+p8   75   0 : N /FIXED
+p9   25 100 : N /FIXED
+p10  75 100 : N /FIXED
---
+++ data/sample/sample.pl
@@ -0,0 +1,21 @@
+UCLA pl 1.0
+# Created: 2025-03-31 -- Generated from image netlist
+# All nodes initially placed at (0,0) as requested
+
+# Movable Cells (Placed at 0,0) - name x y : orientation 
+a   10  10 : N 
+b   10  10 : N 
+c   10  10 : N 
+d   10  10 : N 
+e   10  10 : N 
+f   10  10 : N 
+g   10  10 : N 
+h   10  10 : N 
+i   10  10 : N 
+j   10  10 : N 
+k   10  10 : N 
+l   10  10 : N 
+m   10  10 : N 
+n   10  10 : N 
+o   10  10 : N 
+p   10  10 : N 
